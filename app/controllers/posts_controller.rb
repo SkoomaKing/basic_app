a230549b496d0fc8e4dcecc515deb153c3abc6b8
Caleb Burrus <calebburrus@gmail.com>
2023-12-18 15:52:57 -0500

    added get all posts to controller and routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,10 @@
 class PostsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  def get
+    render json: Post.all.to_json
+  end
+
   def create
     new_post = Post.create(user_id: params[:user_id], content: params[:content])
     render json: {content: new_post}
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/', to: 'static_pages#index'
 
+  get '/posts', to: 'posts#get'
   post '/posts', to: 'posts#create'
 end
