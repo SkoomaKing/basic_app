15388f828447ef4ede03d9408305ba15f270871e
Caleb Burrus <calebburrus@gmail.com>
2023-12-18 15:05:07 -0500

    user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_18_194127) do
+ActiveRecord::Schema.define(version: 2023_12_18_200307) do
 
   create_table "posts", force: :cascade do |t|
     t.string "content"
     t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
