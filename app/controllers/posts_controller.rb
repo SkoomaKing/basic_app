66bb523fa07b1072d369c78c8bc86e5bffee605e
Caleb Burrus <calebburrus@gmail.com>
2023-12-19 14:33:55 -0500

    update and destroy added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,22 @@
 class PostsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-  def list_posts
+  def index
     render json: Post.all.to_json
   end
 
   def create
     new_post = Post.create(user_id: params[:user_id], content: params[:content])
-    render json: {content: new_post}
+    render json: { content: new_post }
+  end
+
+  def update
+    modify_post = Post.update(params[:id], content: params[:content])
+    render json: { content: modify_post }
+  end
+
+  def destroy
+    delete_post = Post.delete(params[:id])
+    render json: { content: delete_post }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get '/', to: 'static_pages#index'
 
-  get '/posts', to: 'posts#list_posts'
+  get '/posts', to: 'posts#index'
   post '/posts', to: 'posts#create'
+  put '/posts/:id', to: 'posts#update'
+  delete '/posts/:id', to: 'posts#destroy'
 end
